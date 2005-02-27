5273e341c75c468e58f212bb5b665163e8792a12
Tobi Oetiker <tobi@oetiker.ch>
2005-02-27 23:26:47 +0000

    removed space ... from makefile

--- lib/Smokeping.pm
+++ lib/Smokeping.pm
@@ -20,7 +20,7 @@ use Smokeping::RRDtools;
 
 # globale persistent variables for speedy
 use vars qw($cfg $probes $VERSION $havegetaddrinfo $cgimode);
-$VERSION="1.99001";
+$VERSION="1.99003";
 
 # we want opts everywhere
 my %opt;
