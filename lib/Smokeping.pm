2ad7c535303ab45e5238507d2d7313ba1275c153
Tobi Oetiker <tobi@oetiker.ch>
2006-05-31 23:00:58 +0000

    prepare for the release of smokeping-20060601_trunk

--- lib/Smokeping.pm
+++ lib/Smokeping.pm
@@ -27,7 +27,7 @@ use Smokeping::RRDtools;
 
 # globale persistent variables for speedy
 use vars qw($cfg $probes $VERSION $havegetaddrinfo $cgimode);
-$VERSION="20060531";
+$VERSION="20060601";
 
 # we want opts everywhere
 my %opt;
