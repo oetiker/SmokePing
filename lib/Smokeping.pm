e940342e10e11fdc8142e18bc64f31494c1268d3
Tobi Oetiker <tobi@oetiker.ch>
2007-03-11 12:40:13 +0000

    if max_rtt is set, use it! http://bugs.debian.org/412826

--- lib/Smokeping.pm
+++ lib/Smokeping.pm
@@ -686,8 +686,7 @@ sub findmax ($$) {
         }
 
         $maxmedian{$x} = $cfg->{Presentation}{detail}{max_rtt} 
-                if $cfg->{Presentation}{detail}{max_rtt} and
-		    $maxmedian{$x} > $cfg->{Presentation}{detail}{max_rtt}
+                if $cfg->{Presentation}{detail}{max_rtt}
      };
      return \%maxmedian;    
 }
