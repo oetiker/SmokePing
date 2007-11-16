c8daf4f31084534e58dd90ccdfa88ba2285ec324
Tobi Oetiker <tobi@oetiker.ch>
2007-11-16 21:34:40 +0000

    added more sample data

--- qooxdoo/source/perl/Qooxdoo/Services/Smokeping.pm
+++ qooxdoo/source/perl/Qooxdoo/Services/Smokeping.pm
@@ -16,12 +16,22 @@ sub method_get_tree
 		 [ 'fk1/sf1','Sub Folder 2',
 		   'fk1/sf1/f3','File 3',	
 		   'fk1/sf1/f4','File 4',
+		   'fk1/sf1/f6','File 3',	
+		   'fk1/sf1/f7','File 4',
+		   'fk1/sf1/f8','File 3',	
+		   'fk1/sf1/f9','File 4',
+		   'fk1/sf1/f10','File 3',	
+		   'fk1/sf1/f11','File 4',
 		 ], 
                ],
-               [ 'fk24','Folder 2'],
+               [ 'fk24','Folder 2',
+   	         'fk24/sf1/f9','File 1',
+	       ],
 	       [ 'fk3','Folder 3',
                       'fk3/f3','File 33',
-		      'fk3/f4','File 44' 
+		      'fk3/f4','File 44',
+                      'fk3/f6','File 33',
+		      'fk3/f7','File 44', 
 	       ]		
 	   ]
 }
