4197b239883707b5f40433f39c4796e886beaa9b
Tobi Oetiker <tobi@oetiker.ch>
2011-09-25 19:56:34 +0000

    sync version

--- lib/Smokeping.pm
+++ lib/Smokeping.pm
@@ -67,7 +67,7 @@ use Smokeping::RRDtools;
 
 # globale persistent variables for speedy
 use vars qw($cfg $probes $VERSION $havegetaddrinfo $cgimode);
-$VERSION="2.004002";
+$VERSION="2.005000";
 
 # we want opts everywhere
 my %opt;
