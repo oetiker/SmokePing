fec626d9e26dbdce91db6eed42d898eb671a7906
Florian Pritz <bluewind@xinu.at>
2013-09-04 19:19:28 +0200

    Fix syntax error in POD
    Possible perl 5.18 related, I didn't look into that.
    
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- lib/Smokeping.pm
+++ lib/Smokeping.pm
@@ -2632,7 +2632,7 @@ DOC
 The base directory where SmokePing keeps the files related to the DYNAMIC function.
 This directory must be writeable by the WWW server. It is also used for temporary
 storage of slave polling results by the master in 
-L<the master/slave mode|smokeping_master_slave>.
+L<the masterE<sol>slave mode|smokeping_master_slave>.
 
 If this variable is not specified, the value of C<datadir> will be used instead.
 DOC
