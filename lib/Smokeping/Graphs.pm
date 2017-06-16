afb95d5dd350c7dddbcf2fce5a31f970ebe26304
Nick <nick@xfce.org>
2017-06-16 09:38:12 +0200

    Improve smokeping dashboard (#87)
    * Generate unordered list for menu html
    
    List are much easier to style with css
    
    * Add div blocks around images in html layout
    
    <div class="panel">
      <div class="panel-heading">
        <h2>Title</h2>
      </div>
      <div class="panel-body">
        IMG or content
      </div>
    </div>
    
    * Add config option htmltitle to render in div headings
    
    If the option is set to yes; the title is not rendered in
    the rrd image, but added to the html panel as a h2 heading.
    
    * Update filter html
    
    Use a label instead of a div and use classes since this is a
    bit more obvious for styling.
    
    * Use label for hierarchy popup
    
    And add classes for consistency.
    
    * Properly close the div tag
    
    Fix for previous commit
    
    * Add alt to rddtool and smokeping logos
    
    2 errors less in html validator...
    
    * Initial commit of new basepage
    
    * Smaller smokeping and rrdtool logos.
    
    Update the rrdlogo to the one currently used on
    the website (with background that matches the
    sidebar) and make both 100x30px.
    
    * Complete sidebar css.

--- lib/Smokeping/Graphs.pm
+++ lib/Smokeping/Graphs.pm
@@ -249,7 +249,7 @@ sub get_multi_detail ($$$$;$){
                ($end ne 'last' ? ('--end',$end) : ()),
                '--height',$cfg->{Presentation}{detail}{height},
                '--width',$cfg->{Presentation}{detail}{width},
-               '--title',$desc,
+               '--title',$cfg->{Presentation}{htmltitle} ne 'yes' ? $desc : '',
                '--rigid','--upper-limit', $max->{$start},
                '--lower-limit',($cfg->{Presentation}{detail}{logarithmic} ? ($max->{$start} > 0.01) ? '0.001' : '0.0001' : '0'),
                '--vertical-label',$ProbeUnit,
@@ -305,18 +305,23 @@ sub get_multi_detail ($$$$;$){
         } elsif ($mode eq 's') { # classic mode
             $startstr =~ s/\s/%20/g;
             $endstr =~ s/\s/%20/g;
-            $page .= "<div>";
+            $page .= "<div class=\"panel\">";
 #           $page .= (time-$timer_start)."<br/>";
 #           $page .= join " ",map {"'$_'"} @task;
-            $page .= "<br/>";
+            $page .= "<div class=\"panel-heading\"><h2>$desc</h2></div>"
+                if $cfg->{Presentation}{htmltitle} eq 'yes';
+            $page .= "<div class=\"panel-body\">";
             $page .= ( qq{<a href="?displaymode=n;start=$startstr;end=now;}."target=".$q->param('target').'">'
                   . qq{<IMG BORDER="0" SRC="${imghref}_${end}_${start}.png">}."</a>" ); #"
-            $page .= "</div>";
+            $page .= "</div></div>\n";
         } else { # chart mode
-            $page .= "<div>";
+            $page .= "<div class=\"panel\">";
+            $page .= "<div class=\"panel-heading\"><h2>$desc</h2></div>"
+                if $cfg->{Presentation}{htmltitle} eq 'yes';
+            $page .= "<div class=\"panel-body\">";
             $page .= (  qq{<a href="}.lnk($q, (join ".", @$open)).qq{">}
                       . qq{<IMG BORDER="0" SRC="${imghref}_${end}_${start}.png">}."</a>" ); #"
-            $page .= "</div>";
+            $page .= "</div></div>\n";
             
         }
 
