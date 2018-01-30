8c7fb0aaebf6ad425fef0052419ec41c5b0269e9
Tobias Oetiker <tobi@oetiker.ch>
2018-01-30 21:43:12 +0100

    update release script

--- release-smokeping.sh
+++ release-smokeping.sh
@@ -3,14 +3,18 @@ set -e
 [ `git status -s | wc -l` -gt 0 ] && echo "ERROR: commit all changes before release" && exit 1
 VERSION=`cat VERSION`
 mkdir -p conftools
+make clean
 ./bootstrap
-./setup/build-perl-modules.sh /tmp/smokeping-$$-build/thirdparty
-./configure  --enable-maintainer-mode --prefix=/tmp/smokeping-$$-build PERL5LIB=/scratch/rrd-149/lib/perl/5.18.2/x86_64-linux-gnu-thread-multi
+make
+make clean
+touch PERL_MODULES
+make PERL=perl-5.10.1
+./configure  --enable-maintainer-mode --prefix=/tmp/smokeping-$$-build
 make install
 make dist
 echo READY TO SYNC ?
 read XXX
-scp CHANGES smokeping-$VERSION.tar.gz oposs@james:public_html/smokeping/pub
+scp CHANGES smokeping-$VERSION.tar.gz oposs@freddie:public_html/smokeping/pub
 rm -fr /tmp/smokeping-$$-build
 git tag $VERSION
 echo "run 'git push;git push --tags' to sync github"
