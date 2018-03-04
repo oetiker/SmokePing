7b6488cd42d3d7af2900627117611b800d118101
Pete Heist <pete@eventide.io>
2018-03-04 12:55:16 +0100

    Simplify variable initialization in new.

--- lib/Smokeping/probes/IRTT.pm
+++ lib/Smokeping/probes/IRTT.pm
@@ -61,9 +61,7 @@ DOC
 }
 
 sub new ($$$) {
-	my $proto = shift;
-	my $class = ref($proto) || $proto;
-	my $self = $class->SUPER::new(@_);
+	my $self = shift->SUPER::new(@_);
 
 	# no need for this if we run as a cgi (still run at startup)
 	unless ( $ENV{SERVER_SOFTWARE} ) {
