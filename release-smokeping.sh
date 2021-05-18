cfb37fe89c89b55e00c5f143245c893469157707
Devesh Chaturvedi <deveshmanish@gmail.com>
2021-05-18 14:32:02 +0530

    Update release-smokeping.sh

--- release-smokeping.sh
+++ release-smokeping.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
+[ `git status -s | wc -l` -gt 0 ] && echo "ERROR: commit all changes before release" && exit 1
 VERSION=`cat VERSION`
 mkdir -p conftools
+make clean
 ./bootstrap
 ./configure  --enable-maintainer-mode --prefix=/tmp/smokeping-$$-build
 make
@@ -10,5 +12,9 @@ make install
 touch PERL_MODULES
 make PERL=perl-5.10.1 || true
 make dist
-#scp CHANGES smokeping-$VERSION.tar.gz oposs@freddie:public_html/smokeping/pub
+echo READY TO SYNC ?
+read XXX
+scp CHANGES smokeping-$VERSION.tar.gz oposs@freddie:public_html/smokeping/pub
 rm -fr /tmp/smokeping-$$-build
+git tag $VERSION
+echo "run 'git push;git push --tags' to sync github"
