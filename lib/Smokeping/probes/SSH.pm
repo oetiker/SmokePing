a1244391c482d0b0e063a25de095b57cebad9e3f
Gabriel Filion <gabster@lelutin.ca>
2025-08-23 20:39:11 -0400

    SSH probe: add ed25519 type host keys to ssh-keyscan
    this key type has been around for a good number of years already so it
    would be nice to make the probe support it.

--- lib/Smokeping/probes/SSH.pm
+++ lib/Smokeping/probes/SSH.pm
@@ -57,8 +57,8 @@ sub new($$$)
 
     # no need for this if we run as a cgi
     unless ( $ENV{SERVER_SOFTWARE} ) {
-        
-        my $call = "$self->{properties}{binary} -t rsa,ecdsa $self->{properties}{init_host}";
+
+        my $call = "$self->{properties}{binary} -t rsa,ecdsa,ed25519 $self->{properties}{init_host}";
         my $return = `$call 2>&1`;
         if ($return =~ m/$ssh_re/s){
             print "### parsing ssh-keyscan output...OK\n";
@@ -152,7 +152,7 @@ sub targetvars {
         return $class->_makevars($class->SUPER::targetvars, {
            keytype => {
                _doc => "Type of key, used in ssh-keyscan -t I<keytype>",
-	       _re => "[ecdr]sa*",
+	       _re => "(rsa|ecdsa|ed25519)",
                _example => 'ecdsa',
                _default => 'rsa',
            },
